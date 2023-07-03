4c805b7a4b3fc736ef6ab72590c59a486b68cd20
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-07-03 15:07:38 +0200

    added link css and bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap link cdn v 5.3.0 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <!-- link CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <h1>
+        hello
+    </h1>
+</body>
+</html>
+
+
 <?php
 
     $hotels = [
@@ -44,6 +64,31 @@
 
 
 
-<?php
-echo "ciaoo";
-?>
+<ul>
+<?php foreach ($hotels as $infoHotel) { ?>
+    <li>
+        <?php echo $infoHotel['name'] . ' '; ?>
+        -
+        <?php echo $infoHotel['description'] . ' '; ?>
+        //
+        <?php echo $infoHotel['parking']; ?>
+        -
+        <?php echo $infoHotel['vote']; ?>
+        -
+        <?php echo $infoHotel['distance_to_center']; ?>
+    </li>
+<?php } ?>
+</ul>
+
+
+<!-- Descrizione
+Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
+Stampare tutti i nostri hotel con tutti i dati disponibili, iniziate in modo graduale.
+Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+Bonus:
+1 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+NOTA: deve essere possibile utilizzare entrambi i filtri contemporaneamente (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel.
+(modificato) -->
